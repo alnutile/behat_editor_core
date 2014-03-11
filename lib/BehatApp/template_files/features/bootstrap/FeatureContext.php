a733292fffbdac2b5df8dc71bf97a9c68fa34948
Alfred Nutile <alfrednutile@gmail.com>
2014-03-11 13:23:27 -0400

    Update FeatureContext.php
    add mink as the default for the FeatureContext

--- lib/BehatApp/template_files/features/bootstrap/FeatureContext.php
+++ lib/BehatApp/template_files/features/bootstrap/FeatureContext.php
@@ -1,5 +1,4 @@
 <?php
-
 use Behat\Behat\Context\ClosuredContextInterface,
     Behat\Behat\Context\TranslatedContextInterface,
     Behat\Behat\Context\BehatContext,
@@ -7,17 +6,12 @@ use Behat\Behat\Context\ClosuredContextInterface,
 use Behat\Gherkin\Node\PyStringNode,
     Behat\Gherkin\Node\TableNode;
 
-//
-// Require 3rd-party libraries here:
-//
-//   require_once 'PHPUnit/Autoload.php';
-//   require_once 'PHPUnit/Framework/Assert/Functions.php';
-//
+use Behat\MinkExtension\Context\MinkContext;
 
 /**
  * Features context.
  */
-class FeatureContext extends BehatContext
+class FeatureContext extends MinkContext
 {
 
     private $output;
